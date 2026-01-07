3d83b0da51e559e3a6ea2d6d79074763ccaed7c4
CarloMart88 <carlo.martino1988@gmail.com>
2026-01-07 17:10:05 +0100

    feat fetch in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,17 @@
 import React from "react";
+import { useState, useEffect } from "react";
+const baseUrl = `http://localhost:3001/coffees`;
 
 function Home() {
+  const [coffees, setCoffees] = useState([]);
+
+  useEffect(() => {
+    fetch(baseUrl)
+      .then((res) => res.json())
+      .then((data) => setCoffees(data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return <div>Home</div>;
 }
 
