43532727c66437d74b4b107bce9a95e279413890
CarloMart88 <carlo.martino1988@gmail.com>
2026-01-07 15:56:36 +0100

    feat App.jsx routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import Detail from "./pages/Detail";
+import Compare from "./pages/Compare";
+
 function App() {
   return (
     <>
-      <h1>homePage</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/coffees/:id" element={<Detail />} />
+            <Route path="/compare" element={<Compare />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
