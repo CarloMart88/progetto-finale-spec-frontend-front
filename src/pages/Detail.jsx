8ac04d414a0445bccb127e4d733b4af3f07fa73a
CarloMart88 <carlo.martino1988@gmail.com>
2026-01-08 12:24:01 +0100

    feat find right image and fetch in Detail.jsx

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,59 +1,31 @@
-import { NavLink, useParams } from "react-router-dom";
-import { useCoffee } from "../context/CoffeeContext";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Detail() {
   const { id } = useParams();
-  const { coffees } = useCoffee();
+  const [coffee, setCoffee] = useState([]);
 
-  const coffee = coffees.find((c) => c.id === Number(id));
+  useEffect(() => {
+    fetch(`http://localhost:3001/coffees/${id}`)
+      .then((res) => res.json())
+      .then((data) => setCoffee(data.coffee))
+      .catch((err) => console.error(err));
+  }, []);
 
-  // Protezione fondamentale
-  if (!coffee) {
-    return <p>Caricamento...</p>;
-  }
+  if (!coffee) return <p>Caricamento...</p>;
 
   return (
-    <div className="container mt-4 d-flex justify-content-center">
-      <div className="card shadow" style={{ maxWidth: "600px" }}>
-        <img
-          src={coffee.image}
-          alt={coffee.title}
-          className="card-img-top"
-          style={{ objectFit: "cover", height: "350px" }}
-        />
-
-        <div className="card-body">
-          <h2 className="card-title mb-3">{coffee.title}</h2>
-
-          <ul className="list-group list-group-flush mb-3">
-            <li className="list-group-item">
-              <strong>Categoria:</strong> {coffee.category}
-            </li>
-            <li className="list-group-item">
-              <strong>Origine:</strong> {coffee.origin}
-            </li>
-            <li className="list-group-item">
-              <strong>Livello di tostatura:</strong> {coffee.roastLevel}
-            </li>
-            <li className="list-group-item">
-              <strong>Intensità:</strong> {coffee.intensity}/10
-            </li>
-            <li className="list-group-item">
-              <strong>Aroma:</strong> {coffee.aromaNotes}
-            </li>
-            <li className="list-group-item">
-              <strong>Processo:</strong> {coffee.process}
-            </li>
-            <li className="list-group-item">
-              <strong>Prezzo al kg:</strong> {coffee.pricePerKg}€
-            </li>
-          </ul>
-
-          <NavLink to="/" className="btn btn-primary w-100">
-            Torna indietro
-          </NavLink>
-        </div>
-      </div>
+    <div>
+      <h2>{coffee.title}</h2>
+      <img src={coffee.image} alt={coffee.title} />
+      <p>Origine: {coffee.origin}</p>
+      <p>Tostatura: {coffee.roastLevel}</p>
+      <p>Intensità: {coffee.intensity}/10</p>
+      <p>Aroma: {coffee.aromaNotes}</p>
+      <p>Processo: {coffee.process}</p>
+      <p>Prezzo: {coffee.pricePerKg}€</p>
+      <NavLink to="/">torna indietro</NavLink>
     </div>
   );
 }
