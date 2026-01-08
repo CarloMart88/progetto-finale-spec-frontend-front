12d39c042b6fd90c138a0146e11af121dbcfaa5d
CarloMart88 <carlo.martino1988@gmail.com>
2026-01-08 13:00:14 +0100

    feat filteredCoffe

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { useCoffee } from "../context/CoffeeContext";
 
 function Home() {
   const { coffees } = useCoffee();
+  const [search, setSearch] = useState("");
+
+  const filteredCoffe = coffees.filter((c) =>
+    c.title.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div className="container py-4">
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Cerca il tuo aroma...."
+      />
       <div className="row">
         <div className="col-12">
           <ul className="list-unstyled">
-            {coffees.map((c) => (
+            {filteredCoffe.map((c) => (
               <li key={c.id} className="coffee-item my-3 p-3">
                 <h4 className="mb-1">
                   <NavLink className="coffee-link" to={`/coffees/${c.id}`}>
