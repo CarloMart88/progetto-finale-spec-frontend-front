11ff6b3f52aca787b85f4d2e3ed07fc253b6ce98
CarloMart88 <carlo.martino1988@gmail.com>
2026-01-08 12:49:00 +0100

    feat style CSS for Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,25 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { useCoffee } from "../context/CoffeeContext";
-const baseUrl = `http://localhost:3001/coffees`;
 
 function Home() {
   const { coffees } = useCoffee();
 
   return (
-    <div className="container">
+    <div className="container py-4">
       <div className="row">
-        <div className="col-12 ">
-          <ul>
-            {coffees.map((c) => {
-              return (
-                <li key={c.id} className="my-3">
-                  <NavLink to={`/coffees/${c.id}`}>Nome: {c.title}</NavLink>
-                  <p>Categoria: {c.category}</p>
-                </li>
-              );
-            })}
+        <div className="col-12">
+          <ul className="list-unstyled">
+            {coffees.map((c) => (
+              <li key={c.id} className="coffee-item my-3 p-3">
+                <h4 className="mb-1">
+                  <NavLink className="coffee-link" to={`/coffees/${c.id}`}>
+                    {c.title}
+                  </NavLink>
+                </h4>
+                <p className="mb-0 text-muted">Categoria: {c.category}</p>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
