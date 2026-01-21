79402b9a6fa26debf1c94c232fc68a7a4d7c7406
CarloMart88 <carlo.martino1988@gmail.com>
2026-01-21 14:23:02 +0100

    feat push type.ts

---
+++ types.ts
@@ -0,0 +1,13 @@
+export type Coffee = {
+  id: number;
+  title: string;
+  category: string;
+  image: string;
+  origin: string;
+  roastLevel: string;
+  intensity: number;
+  aromaNotes: string;
+  pricePerKg: number;
+  process: string;
+  story:string;
+};
